00eeaa1f890983066081f6dc97de0ce28d033f14
Lewis <lois.motel@pm.me>
2020-01-08 16:52:14 +0100

    Server option and plugin for django

--- emergency/emergency_server/manserver/manserver/settings.py
+++ emergency/emergency_server/manserver/manserver/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'manserver.api',
+    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
--- simulation/simulation_server/simserver/settings.py
+++ simulation/simulation_server/simserver/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'simserver.api',
+    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
